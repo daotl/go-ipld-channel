6e35f65965af33119caa2344f76f15cf1bab3d8a
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2021-06-14 19:17:26 +0800

    feat: add StringsToChannels and ChannelsToStrings funcs

--- channel.go
+++ channel.go
@@ -12,3 +12,21 @@ const (
 func (c Channel) String() string {
 	return string(c)
 }
+
+// StringsToChannels converts a slice of strings to a slice of Channels.
+func StringToChannels(strs []string) []Channel {
+	chns := make([]Channel, 0, len(strs))
+	for i, s := range strs {
+		chns[i] = Channel(s)
+	}
+	return chns
+}
+
+// ChannelsToStrings converts a slice of channels to a slice of strings.
+func ChannelsToStrings(chns []Channel) []string {
+	strs := make([]string, 0, len(chns))
+	for i, chn := range chns {
+		strs[i] = chn.String()
+	}
+	return strs
+}
