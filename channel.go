e643b5463a3964c82616315ae38c1e58b2ace85e
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2021-06-14 16:01:56 +0800

    feat: add Channel.String() method

--- channel.go
+++ channel.go
@@ -1,10 +1,14 @@
 package channel
 
-// Channel represents an channel within which IPLD blocks can be store and
+// Channel represents an IPLD channel within which blocks can be store and
 // exchanged, possibly with access control.
 type Channel string
 
 const (
-	// PublicChannel represents the default public channel without access control.
+	// PublicChannel represents the default public IPLD channel without access control.
 	PublicChannel Channel = ""
 )
+
+func (c Channel) toString() string {
+	return string(c)
+}
--- pair/pair.go
+++ pair/pair.go
@@ -6,12 +6,25 @@ import (
 	channel "github.com/daotl/go-ipld-channel"
 )
 
-// ChannelCidPair is a pair of channel and cid.
+// ChannelCidPair is a pair of IPLD channel and cid.
 type ChannelCidPair struct {
 	Chn channel.Channel
 	Cid cid.Cid
 }
 
+// Defined returns true if a ChannelCidPair is defined
+// Calling any other methods on an undefined ChannelCidPair will result in
+// undefined behavior.
+func (p ChannelCidPair) Defined() bool {
+	return p.Cid.Defined()
+}
+
+// Equals checks that two ChannelCidPairs are the same.
+func (p ChannelCidPair) Equals(o ChannelCidPair) bool {
+	return p == o
+}
+
+// PublicCidPair returns a pair consisting of the public IPLD channel and the specified cid.
 func PublicCidPair(c cid.Cid) ChannelCidPair {
 	return ChannelCidPair{
 		Chn: channel.PublicChannel,
@@ -19,29 +32,17 @@ func PublicCidPair(c cid.Cid) ChannelCidPair {
 	}
 }
 
-// Defined returns true if a ChannelCidPair is defined
-// Calling any other methods on an undefined ChannelCidPair will result in
-// undefined behavior.
-func (k ChannelCidPair) Defined() bool {
-	return k.Cid.Defined()
-}
-
-// Equals checks that two ChannelCidPairs are the same.
-func (k ChannelCidPair) Equals(o ChannelCidPair) bool {
-	return k == o
-}
-
 func CidsToPairs(cids []cid.Cid, chn channel.Channel) []ChannelCidPair {
-	keys := make([]ChannelCidPair, 0, len(cids))
+	pairs := make([]ChannelCidPair, 0, len(cids))
 	for _, c := range cids {
-		keys = append(keys, ChannelCidPair{chn, c})
+		pairs = append(pairs, ChannelCidPair{chn, c})
 	}
-	return keys
+	return pairs
 }
 
-func PairsToCids(keys []ChannelCidPair) []cid.Cid {
-	cids := make([]cid.Cid, 0, len(keys))
-	for _, key := range keys {
+func PairsToCids(pairs []ChannelCidPair) []cid.Cid {
+	cids := make([]cid.Cid, 0, len(pairs))
+	for _, key := range pairs {
 		cids = append(cids, key.Cid)
 	}
 	return cids
